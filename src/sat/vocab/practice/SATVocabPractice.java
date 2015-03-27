1b53c87dbd7c7fefd5b729464069c197263726c4
Tavares42127 <armor421@gmail.com>
2015-03-27 14:56:24 -0400

    Imported Scanner

--- src/sat/vocab/practice/SATVocabPractice.java
+++ src/sat/vocab/practice/SATVocabPractice.java
@@ -17,7 +17,7 @@ import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Scanner;
 import java.util.Random;
 //import java.util.Scanner;
 public class SATVocabPractice {

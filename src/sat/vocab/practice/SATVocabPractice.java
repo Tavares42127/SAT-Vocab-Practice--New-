04d1e21d6782c57b54f31fb39eb8f704705b95a0
jordanv15 <lighthouse3715@hotmail.com>
2015-04-02 16:28:20 -0400

    Reworked Main Class

--- src/sat/vocab/practice/SATVocabPractice.java
+++ src/sat/vocab/practice/SATVocabPractice.java
@@ -18,38 +18,36 @@ package sat.vocab.practice;
 //import java.util.Random;
 public class SATVocabPractice 
 {
-    private String word;
-    private String definition;
-    
+ import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class X {
     public static void main(String[] args) {
-        int[] questionNumbersSorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-        suffleArray(questionNumbersSorted); //Test
-   
-    public String getWord() //Get a word from the file
-    {
-        return word;
-    }
+    File f = new File("Words.txt");
+        try{
+            ArrayList<String> lines = get_arraylist_from_file(f);
+            for(int x = 0; x < lines.size(); x++){
+                System.out.println(lines.get(x));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Done");
 
-    public void setWord(String word) //Set the word
-    {
-        this.word = word;
     }
-
-    public String getDefinition() //Retrieve definition
-    {
-        return definition;
+    public static ArrayList<String> get_arraylist_from_file(File f) 
+        throws FileNotFoundException {
+        Scanner s;
+        ArrayList<String> list = new ArrayList<String>();
+        s = new Scanner(f);
+        while (s.hasNext()) {
+            list.add(s.next());
+        }
+        s.close();
+        return list;
     }
-    }
-    //static void suffleArray(int[] ar){
-      //Random rnd = new Random();
-        //for (int i = ar.length - 1; i > 0; i--)
-        //{
-            //int index = rnd.nextInt(i + 1);
-          //int a = ar[index];
-            //ar[index] = ar[i];
-            //ar[i] = a;
-        //}
-    //}
-        //}
-    
-
+}   
+}

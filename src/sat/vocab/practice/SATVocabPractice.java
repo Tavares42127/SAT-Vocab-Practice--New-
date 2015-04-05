c42a46f42f0801a880d6a79355fa40fe145588d2
Tavares42127 <armor421@gmail.com>
2015-04-05 19:18:11 -0400

    Fixed spelling error/ tried array fix.
    array fix did not work

--- src/sat/vocab/practice/SATVocabPractice.java
+++ src/sat/vocab/practice/SATVocabPractice.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-
-package sat.vocab.practice;
-
-/**
- *
- * @author Brandon Tavares
- * 
- */
-
-import java.io.FileReader;
-import com.opencsv.CSVReader;
-import java.util.Arrays;
-
-public class SATVocabPractice {
-    @SuppressWarnings("resource")
-   public static void main(String[] args) throws Exception
-   {
-       
-      //Build reader instance
-      //Read data.csv
-      //Default seperator is comma
-      //Default quote character is double quote
-      //Start reading from line number 2 (line numbers start from zero)
-      CSVReader reader = new CSVReader(new FileReader("C:/Users/Brandon/Documents/NetBeansProjects/SAT Vocab Practice/src/sat/vocab/practice/Words.txt"), '|' , '"' , 0);
-       
-      //Read CSV line by line and use the string array as you want
-      String[] nextLine;
-      while ((nextLine = reader.readNext()) != null) {
-         if (nextLine != null) {
-            //Verifying the read data here
-            System.out.println(Arrays.toString(nextLine));
-         }
-       }
-   }
-} 
-=======
-
 package sat.vocab.practice;
 
 /**
@@ -62,7 +24,7 @@ public class SATVocabPractice {
       //Default seperator is comma
       //Default quote character is double quote
       //Start reading from line number 2 (line numbers start from zero)
-      CSVReader reader = new CSVReader(new FileReader("C:/Users/Jordan/Documents/GitHub/SAT-Vocab-Practice--New-/src/sat/vocab/practice/Words.txt"), '|' , '"' , 0);
+      CSVReader reader = new CSVReader(new FileReader("C:/Users/Brandon/Documents/NetBeansProjects/SAT Vocab Practice/src/sat/vocab/practice/Words.txt"), '|' , '"' , 0);
        
       //Read CSV line by line and use the string array as you want
       String[] nextLine;
@@ -74,8 +36,8 @@ public class SATVocabPractice {
             words.add(Arrays.toString(nextLine).split("\\|"));
          }
        }
-            System.out.println(words); //Print the word list
-            System.out.println(definitions); //Print the definitions
+            System.out.println(words.toString()); //Print the word list
+            System.out.println(definitions.toString()); //Print the definitions
    }
 } 
->>>>>>> origin/master
+

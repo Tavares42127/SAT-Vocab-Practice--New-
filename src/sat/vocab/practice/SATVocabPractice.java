cb1c8b386a283bd98c3a4c924805c4fb59de4b21
Tavares42127 <armor421@gmail.com>
2015-04-05 22:31:42 -0400

    my bs

--- src/sat/vocab/practice/SATVocabPractice.java
+++ src/sat/vocab/practice/SATVocabPractice.java
@@ -6,38 +6,35 @@ package sat.vocab.practice;
  * 
  */
 
-import java.io.FileReader;
-import com.opencsv.CSVReader;
-import java.util.Arrays;
-import java.util.ArrayList;
-
-public class SATVocabPractice {
-    @SuppressWarnings("resource")
-   public static void main(String[] args) throws Exception
-   {
-       
-       ArrayList words = new ArrayList(); //Word arrays 
-       ArrayList definitions = new ArrayList(); //Definition arrays. 
-       
-      //Build reader instance
-      //Read data.csv
-      //Default seperator is comma
-      //Default quote character is double quote
-      //Start reading from line number 2 (line numbers start from zero)
-      CSVReader reader = new CSVReader(new FileReader("C:/Users/Brandon/Documents/NetBeansProjects/SAT Vocab Practice/src/sat/vocab/practice/Words.txt"), '|' , '"' , 0);
-       
-      //Read CSV line by line and use the string array as you want
-      String[] nextLine;
-      while ((nextLine = reader.readNext()) != null) {
-         if (nextLine != null) {
-            //Verifying the read data here
-            System.out.println(Arrays.toString(nextLine));
-            definitions.add(Arrays.toString(nextLine).split("|"));
-            words.add(Arrays.toString(nextLine).split("\\|"));
-         }
-       }
-            System.out.println(words.toString()); //Print the word list
-            System.out.println(definitions.toString()); //Print the definitions
-   }
-} 
+String[] definitions = {
+"n: 1. an intricate combination of paths or passages in which it is difficult to find one's way or to reach the exit.",
+"v: 1. to crouch, as in fear or shame.",
+"adj: 1. made in imitation so as to be passed off fraudulently or deceptively as genuine; not genuine; forged.",
+"n: 1. a stone, ring, or other object, engraved with figures or characters supposed to possess occult powers and worn as an amulet or charm.",
+"adj: 1. tending to annoy or cause ill will; overly aggressive.",
+"adj: 1. concealed; secret; disguised.",
+"v: 1. to regard with extreme repugnance or aversion; detest utterly; loathe; abominate.",
+"v: 1. to render or declare legally void or inoperative.",
+"n: 1. a puzzling or inexplicable occurrence or situation.",
+"n: 1.  a rough, jagged tear.",
+"v: 1. to bring (something) to an end or to completion; complete",
+"v: 1. to throw (a person or thing) out of a window.",
+"adj: 1. (of finery, trappings, etc.) gaudy; showy and cheap.",
+"v: 1. to delay the impending punishment or sentence of (a condemned person).",
+"adj: 1. lasting a very short time; short-lived; transitory.",
+"adj: 1. of the nature of or made or done as a trial, experiment, or attempt; experimental.",
+"adj: 1. mysterious in meaning; puzzling; ambiguous.",
+"v: 1. to abrogate; annul; revoke; repeal.",
+"v: 1. to hold oneself back voluntarily, especially from something regarded as improper or unhealthy (usually followed by from).",
+"adj: 1. sharp or biting to the taste or smell; bitterly pungent; irritating to the eyes, nose, etc.",
+"adj: 1. keenly distressing to the feelings.",
+"n: 1. a pathological fear of heights.",
+"n: 1. a figure or symbol with a hidden meaning.",
+"n: 1. the act or power of remembering things; memory.",
+"v: 1. to cause delay, interruption, or difficulty in; hamper; impede.",
+"adj: 1. no longer in general use; fallen into disuse.",
+"adj: 1. deviating from the usual or proper course in conduct or opinion; eccentric; queer.",
+"v: 1. to indicate in advance; to foreshadow or presage, as an omen does.",
+"v: 1. to regard with respect tinged with awe; venerate.",
+"n: 1. a wall of earth or other material built for defense; rampart."};
 

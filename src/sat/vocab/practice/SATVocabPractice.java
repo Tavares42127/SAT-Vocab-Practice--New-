803b95cb2d0ee8e7ea1d852e901cecc59ea6224c
jordanv15 <lighthouse3715@hotmail.com>
2015-04-05 20:34:11 -0400

    Fixed Issue

--- src/sat/vocab/practice/SATVocabPractice.java
+++ src/sat/vocab/practice/SATVocabPractice.java
@@ -12,17 +12,23 @@ import com.opencsv.CSVReader;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+
+
 public class SATVocabPractice {
     @SuppressWarnings("resource")
+    
+    public int totalWords = 30;
+    
    public static void main(String[] args) throws Exception
    {
+       
        
        ArrayList words = new ArrayList(); //Word arrays 
        ArrayList definitions = new ArrayList(); //Definition arrays. 
        
       //Build reader instance
       //Read data.csv
-      //Default seperator is comma
+      //Default seperator is "|"
       //Default quote character is double quote
       //Start reading from line number 2 (line numbers start from zero)
       CSVReader reader = new CSVReader(new FileReader("C:/Users/Jordan/Documents/GitHub/SAT-Vocab-Practice--New-/src/sat/vocab/practice/Words.txt"), '|' , '"' , 0);
@@ -33,8 +39,8 @@ public class SATVocabPractice {
          if (nextLine != null) {
             //Verifying the read data here
             System.out.println(Arrays.toString(nextLine));
-            definitions.add(Arrays.toString(nextLine).split("|"));
-            words.add(Arrays.toString(nextLine).split("\\|"));
+            definitions.add(Arrays.toString(nextLine));
+            words.add(Arrays.toString(nextLine));
          }
        }
             System.out.println(words); //Print the word list
